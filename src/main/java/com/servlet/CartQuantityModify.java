b48eb5b78e603605c7b91f7c0fd39238b205dae0
LesegoLSG <217057620@student.uj.ac.za>
2023-05-17 17:41:46 -0500

    Added a servlet to increment and decrement product price according to the quantity

---
+++ src/main/java/com/servlet/CartQuantityModify.java
@@ -0,0 +1,36 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CartQuantityModify
+ */
+@WebServlet("/CartQuantityModify")
+public class CartQuantityModify extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+ 
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.print("Increment and decrement");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
